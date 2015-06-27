61142dc1444a6eeec34d1f207141915e1324dc19
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-06-27 19:19:40 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- GAMeasurementProtocol.php
+++ GAMeasurementProtocol.php
@@ -11,7 +11,7 @@
 class GAMeasurementProtocol
 {
   const URL = 'http://www.google-analytics.com/collect';
-  const ANONYMOUS_CID = 555;  // Anonymous Client ID.
+  const ANONYMOUS_CID = 555; // Anonymous Client ID.
   private $trackingID = null;
   private $domain     = null;
   
@@ -22,7 +22,7 @@ class GAMeasurementProtocol
    * @param string $domainName e.g. freshsauce.co.uk
    * @author Dave Barnwell
    */
-  public function __construct($trackingID,$domainName) {
+  public function __construct($trackingID, $domainName) {
     $this->trackingID = $trackingID;
     $this->domain     = $domainName;
   }
@@ -32,20 +32,20 @@ class GAMeasurementProtocol
       'v'   => 1,
       'tid' => urlencode($this->trackingID),
       'cid' => self::ANONYMOUS_CID,
-      'z'   => rand(0,100000)   // cache buster, but shouldnt be needed as server side
+      'z'   => rand(0, 100000)   // cache buster, but shouldnt be needed as server side
     );
   }
 
 
-  public function trackTransactionHit($transId,$affiliationName='shop name',$amount=1,$shipping=0,$tax=0,$curreny='GBP') {
+  public function trackTransactionHit($transId, $affiliationName = 'shop name', $amount = 1, $shipping = 0, $tax = 0, $curreny = 'GBP') {
     $fields        = $this->commonFields();
-    $fields['t']   = 'transaction';                        // Transaction hit type.
-    $fields['ti']  = urlencode($transId);                  // transaction ID. Required.
-    $fields['ta']  = urlencode($affiliationName);          // Transaction affiliation. (shop name)
-    $fields['tr']  = urlencode(sprintf('%.2f',$amount));   // Transaction revenue.
-    $fields['ts']  = urlencode(sprintf('%.2f',$shipping)); // Transaction shipping.
-    $fields['tt']  = urlencode(sprintf('%.2f',$tax));      // Transaction tax.
-    $fields['cu']  = urlencode($curreny);                  // ISO Currency code.
+    $fields['t']   = 'transaction'; // Transaction hit type.
+    $fields['ti']  = urlencode($transId); // transaction ID. Required.
+    $fields['ta']  = urlencode($affiliationName); // Transaction affiliation. (shop name)
+    $fields['tr']  = urlencode(sprintf('%.2f', $amount)); // Transaction revenue.
+    $fields['ts']  = urlencode(sprintf('%.2f', $shipping)); // Transaction shipping.
+    $fields['tt']  = urlencode(sprintf('%.2f', $tax)); // Transaction tax.
+    $fields['cu']  = urlencode($curreny); // ISO Currency code.
     return self::sendData($fields);
   }
   
@@ -56,19 +56,19 @@ class GAMeasurementProtocol
    * @param string $items 
    * @return bool
    */
-  public function trackItemHits($transId,$items) {
+  public function trackItemHits($transId, $items) {
     if ($items && is_array($items)) {
-      $i=1;
-      foreach($items as $item) {
+      $i = 1;
+      foreach ($items as $item) {
         $fields        = $this->commonFields();
-        $fields['t']   = 'item';                                     // purchase on page.
-        $fields['ti']  = urlencode($transId);                        // transaction ID. Required.
-        $fields['in']  = $item['name'];                              // product name.
-        $fields['ip']  = urlencode(sprintf('%.2f',$item['price']));  // product price
-        $fields['iq']  = $item['quantity'];                          // product quantity
-        $fields['ic']  = $item['sku'];                               // product code /SKU
-        $fields['iv']  = $item['variant'];                           // product variant/category
-        $fields['cu']  = $item['curreny'];                           // product position.
+        $fields['t']   = 'item'; // purchase on page.
+        $fields['ti']  = urlencode($transId); // transaction ID. Required.
+        $fields['in']  = $item['name']; // product name.
+        $fields['ip']  = urlencode(sprintf('%.2f', $item['price'])); // product price
+        $fields['iq']  = $item['quantity']; // product quantity
+        $fields['ic']  = $item['sku']; // product code /SKU
+        $fields['iv']  = $item['variant']; // product variant/category
+        $fields['cu']  = $item['curreny']; // product position.
         if (!self::sendData($fields)) return false;
       }
     }
@@ -76,46 +76,46 @@ class GAMeasurementProtocol
   }
   
   
-  public function trackPageView($pageUrl,$pageTitle='') {
+  public function trackPageView($pageUrl, $pageTitle = '') {
     $fields        = $this->commonFields();
-    $fields['t']   = 'pageview';                 // Pageview hit type.
-    $fields['dh']  = urlencode($this->domain);   // Document hostname.
-    $fields['dp']  = urlencode($pageUrl);        // Page.
-    $fields['dt']  = urlencode($pageTitle);      // Title.
+    $fields['t']   = 'pageview'; // Pageview hit type.
+    $fields['dh']  = urlencode($this->domain); // Document hostname.
+    $fields['dp']  = urlencode($pageUrl); // Page.
+    $fields['dt']  = urlencode($pageTitle); // Title.
     return self::sendData($fields);
   }
   
-  public function trackEvent($category,$action,$label='',$value=0) {
+  public function trackEvent($category, $action, $label = '', $value = 0) {
     $fields        = $this->commonFields();
-    $fields['t']   = 'event';                // Event hit type
-    $fields['ec']  = urlencode($category);   // Event Category. Required.
-    $fields['ea']  = urlencode($action);     // Event Action. Required.
-    $fields['el']  = urlencode($label);      // Event label.
-    $fields['ev']  = urlencode($value);      // Event value.
+    $fields['t']   = 'event'; // Event hit type
+    $fields['ec']  = urlencode($category); // Event Category. Required.
+    $fields['ea']  = urlencode($action); // Event Action. Required.
+    $fields['el']  = urlencode($label); // Event label.
+    $fields['ev']  = urlencode($value); // Event value.
     return self::sendData($fields);
   }
   
-  public function trackException($description,$fatal) {
+  public function trackException($description, $fatal) {
     $fields        = $this->commonFields();
-    $fields['t']   = 'exception';               // Exception hit type
-    $fields['exd'] = urlencode($description);   // Exception description.
+    $fields['t']   = 'exception'; // Exception hit type
+    $fields['exd'] = urlencode($description); // Exception description.
     $fields['exf'] = urlencode($fatal ? 1 : 0); // Exception is fatal?
     return self::sendData($fields);
   }
   
   private static function sendData($fields) {
     //url-ify the data for the POST
-    foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+    foreach ($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
     rtrim($fields_string, '&');
 
     //open connection
     $ch = curl_init();
 
     //set the url, number of POST vars, POST data
-    curl_setopt($ch,CURLOPT_URL, self::URL);
-    curl_setopt($ch,CURLOPT_POST, count($fields));
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+    curl_setopt($ch, CURLOPT_URL, self::URL);
+    curl_setopt($ch, CURLOPT_POST, count($fields));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
 
     //execute post
     $result = curl_exec($ch);
